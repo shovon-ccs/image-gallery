b815be004d1b87b60c0cd680f055d5509484fa3e
Shovon Roy <shovonroy@Shovons-MacBook-Air.local>
2025-11-11 21:59:34 +0600

    enhanced home page layout with improved styling and background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const HomePage = () => {
   return (
-    <main className='flex justify-center items-center h-screen'>
-      <h1 className='text-green-500 text-5xl font-bold'>Home Page</h1>
+    <main className='flex justify-center items-center min-h-screen bg-sky-200'>
+      <div className='w-full max-w-7xl mx-auto p-20 bg-slate-100 rounded-2xl shadow-lg'>
+        <h1 className='text-green-500 text-5xl font-bold'>Home Page</h1>
+      </div>
+      
     </main>
   )
 }

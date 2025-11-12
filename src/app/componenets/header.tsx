80ffe0213b60e5ff17bdbc21fe6a73ab1324cef2
Shovon Roy <shovonroy@Shovons-MacBook-Air.local>
2025-11-12 20:27:01 +0600

    add header component with search functionality and integrate it into the home page

---
+++ src/app/componenets/header.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import SearchIcon from './icons'
+
+const Header = () => {
+  return (
+    <div className='flex justify-between items-center gap-2.5'>
+        <div className='w-full max-w-xl flex justify-center items-center'>
+            <input type="text" placeholder='Search...'
+            className='inline-block w-full py-2 px-4 pr-8 bg-slate-100 border-2 border-slate-400 focus:outline-none text-slate-500 
+            rounded'/>
+            <button className='w-8 -ml-8'>
+                <SearchIcon />
+            </button>
+
+        </div>
+        <button className='px-8 bg-slate-800 text-white text-base uppercase font-bold py-4 rounded-xl 
+        hover:bg-slate-700 active:translate-y-0.5 cursor-pointer'>
+            Upload
+        </button>
+
+    </div>
+  )
+}
+
+export default Header
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavBar from './componenets/nav/index'
+import Header from './componenets/header'
 
 const HomePage = () => {
   return (
     <main className='flex justify-center items-center min-h-screen bg-sky-200'>
       <div className='w-full max-w-7xl mx-auto p-20 bg-slate-100 rounded-2xl shadow-lg'>
         <NavBar />
+        <Header />
         <h1 className='text-green-500 text-5xl font-bold'>Home Page</h1>
       </div>
       
